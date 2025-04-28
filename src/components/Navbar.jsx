22344be36004e4732671f14a920461b2d5a3f026
lukman8840 <lukmansaid4280@gmail.com>
2025-04-28 08:11:37 +0100

    Navbar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
+import { FaSignInAlt } from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+      <div className="left-section">
+        <div className="logo">
+           <h1>LearnFlow</h1>
+      </div>
+        <div className="nav-links">
+            <a href="#home">Home</a>
+            <a href="#courses">Courses</a>
+            <a href="#About">About</a>
+            <a href="#contact">Contact</a>
+        </div>
+      </div>
+        <div className="right-section">
+        <button className="login">
+          Login
+          <FaSignInAlt style={{marginLeft: '18px'}}/>
+          </button>
+        </div>
+      </div>
+  )
+}
+
+export default Navbar
