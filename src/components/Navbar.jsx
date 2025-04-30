bcb681238fd854b151b16608afa30e1fa37e1801
lukman8840 <lukmansaid4280@gmail.com>
2025-04-30 14:30:09 +0100

    dropdown courses added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Navbar.css'
 import { FaSignInAlt } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false)
+
   return (
     <div className='nav-container'>
       <div className="left-section">
@@ -12,7 +14,23 @@ const Navbar = () => {
         </div>
         <div className="nav-links">
             <Link to="/">Home</Link>
-            <Link to="/courses">Courses</Link>
+
+            <div className='dropdown'
+              onMouseEnter={() => setShowDropdown(true)}
+              onMouseLeave={() => setShowDropdown(false)}
+            >
+              <span className='dropdown-toggle'>Courses ▾</span>
+              {showDropdown && (
+                <div className='dropdown-menu'>
+                  <Link to="/courses/gmail">Gmail Basics</Link>
+                  <Link to="/courses/gmail">Google Docs</Link>
+                  <Link to="/courses/gmail">Internet Research</Link>
+                  <Link to="/courses/gmail">Intro to AI Tools</Link>
+                  <Link to="/courses/gmail">File Storage</Link>
+                  <Link to="/courses/gmail">Online Safety</Link>
+                </div>
+              )}
+            </div>
             <Link to="/About">About</Link>
             <Link to="/contact">Contact</Link>
         </div>
