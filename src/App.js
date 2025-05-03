c7ecf08b8303a78065ea49072659abca909afee4
lukman8840 <lukmansaid4280@gmail.com>
2025-05-03 15:11:17 +0100

    forget password form added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Contact from './components/pages/Contact';
 import LoginForm from './components/LoginForm';
 import SignUpForm from './components/SignUpForm';
 import Navbar from './components/Navbar';
+import ForgetPassword from './components/ForgetPassword';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path='/contact' element={<Contact />} />
         <Route path='/login' element={<LoginForm />} />
         <Route path='/signup' element={<SignUpForm />}/>
+        <Route path='/forget-password' element={<ForgetPassword />}/>
       </Routes>      
     </Router>
   );
---
+++ src/components/ForgetPassword.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+const ForgetPassword = () => {
+    const [email, setEmail] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmitted(true);
+        console.log("Request Submitted to", email)
+    }
+  return (
+    <div className='auth-container'>
+        <h2>Reset Your Password</h2>
+        {!submitted ? (
+            <form onSubmit={handleSubmit} className='auth-form'>
+                <input 
+                    type='email' 
+                    placeholder='Enter The Email You Registered with'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+        <button>Send Reset Link</button>
+    </form>
+
+) : (
+    <p style={{ textAlign: 'center', color: '#4f8ef7'}}>
+         If an account with this email exists, a password reset link has been sent.
+    </p>
+)}
+    <div className="authcontainer">
+        <p>Remebered Your password? <Link to='/login'>Back to Login</Link></p>
+    </div>
+</div>
+  )
+}
+
+export default ForgetPassword
